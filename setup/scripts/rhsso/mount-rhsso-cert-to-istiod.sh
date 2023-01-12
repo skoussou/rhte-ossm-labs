e6b737845d784b69c6f7e8b15e00c6eb4feb31fd
skoussou <skousou@gmail.com>
2023-01-12 09:22:21 +0000

    reducing the time to mount rhsso certs to istiod to speed up scenario-4 start

--- setup/scripts/rhsso/mount-rhsso-cert-to-istiod.sh
+++ setup/scripts/rhsso/mount-rhsso-cert-to-istiod.sh
@@ -29,7 +29,7 @@ echo "Create a secret from this CA certificate in $SM_CP_NS project"
 echo "-----------------------------------------------------------------------------"
 oc -n $SM_CP_NS delete secret/openshift-wildcard
 echo "oc -n $SM_CP_NS create secret generic openshift-wildcard --from-file=extra.pem=/tmp/$SM_TENANT_NAME/tls.crt"
-sleep 2
+#sleep 2
 oc -n $SM_CP_NS create secret generic openshift-wildcard --from-file=extra.pem=/tmp/$SM_TENANT_NAME/tls.crt
 sleep 1
 echo
@@ -39,9 +39,9 @@ echo "--------------------------------------------------------------------------
 oc set volume -n $SM_CP_NS deployment/istiod-$SM_TENANT_NAME --remove --name=extracacerts --containers=discovery
 echo
 echo
-sleep 5
+sleep 2
 echo "oc set volumes -n $SM_CP_NS deployment/istiod-$SM_TENANT_NAME --add  --name=extracacerts  --mount-path=/cacerts  --secret-name=openshift-wildcard  --containers=discovery"
-sleep 3
+#sleep 3
 oc set volumes -n $SM_CP_NS deployment/istiod-$SM_TENANT_NAME --add  --name=extracacerts  --mount-path=/cacerts  --secret-name=openshift-wildcard  --containers=discovery
 sleep 5
 echo
@@ -56,7 +56,7 @@ echo "podname=[$podname]"
 echo
 #podname=$(echo podname| sed '1d')
 #echo "2nd time [$podname]"
-sleep 5
+sleep 2
 
 # RSH to istiod pod
 #echo "oc -n $SM_CP_NS rsh $podname"
@@ -71,12 +71,12 @@ oc -n $SM_CP_NS exec $podname -- curl -I https://keycloak-rhsso.apps.$CLUSTERNAM
 #curl: (60) SSL certificate problem: self signed certificate in certificate chain
 echo
 echo
-sleep 7
+sleep 3
 
 echo "Check connection to RHSSO with the CA"
 #[pod] sh-4.4$ curl --cacert /cacerts/extra.pem -I https://keycloak-rhsso.apps.<CLUSTERNAME>.<BASEDOMAIN>/auth/
 echo "oc -n $SM_CP_NS exec $podname -- curl --cacert /cacerts/extra.pem -I https://keycloak-rhsso.apps.$CLUSTERNAME.$BASEDOMAIN/auth/"
-sleep 7
+sleep 3
 oc -n $SM_CP_NS exec $podname -- curl --cacert /cacerts/extra.pem -I https://keycloak-rhsso.apps.$CLUSTERNAME.$BASEDOMAIN/auth/
 #HTTP/1.1 200 OK
 echo
