de2fa0d66f9f8d11807b8cca5147a3057046a84a
skoussou <skousou@gmail.com>
2023-01-13 11:35:53 +0000

    scenario-5 addeed completion script

---
+++ lab-5/complete-lab-5.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+
+SSO_CLIENT_SECRET=$1
+OCP_DOMAIN=$2 #apps.cluster-w4h2j.w4h2j.sandbox2385.opentlc.com
+LAB_PARTICIPANT_ID=$3
+
+
+echo
+echo "---Values Used-----------------------------------------------"
+echo "OCP_DOMAIN:            $OCP_DOMAIN"
+echo "LAB_PARTICIPANT_ID:    $LAB_PARTICIPANT_ID"
+echo "-------------------------------------------------------------"
+set -e
+
+sleep 5
+
+echo
+echo "Task 3: Applying default authorization policies"
+echo "#################################################"
+echo
+echo
+sleep 3
+
+echo "Step 1 - Verify current default AUTHZ is ALLOW all"
+echo "-----------------------------------------------------------"
+sleep 3
+
+./login-as.sh emma
+echo
+echo "----------"
+
+./check-authz-all.sh ALLOW user-$LAB_PARTICIPANT_ID-prod-istio-system $OCP_DOMAIN $SSO_CLIENT_SECRET $LAB_PARTICIPANT_ID
+echo
+echo
+echo
+echo
+sleep 2
+
+echo "Step 2 - Apply best practice security pattern to DENY all"
+echo "-----------------------------------------------------------"
+
+
+./login-as.sh farid
+echo
+echo "----------"
+echo "apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+  name: allow-nothing
+  namespace: user-$LAB_PARTICIPANT_ID-prod-travel-agency
+spec:
+  {}"
+echo
+
+echo "apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+  name: allow-nothing
+  namespace: user-$LAB_PARTICIPANT_ID-prod-travel-agency
+spec:
+  {}" | oc apply -f -
+
+
+./login-as.sh cristina
+echo
+echo "----------"
+echo "apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+  name: allow-nothing
+  namespace: user-$LAB_PARTICIPANT_ID-prod-travel-control
+spec:
+  {}  "
+echo
+
+echo "apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+  name: allow-nothing
+  namespace: user-$LAB_PARTICIPANT_ID-prod-travel-control
+spec:
+  {}  " | oc apply -f -
+
+echo
+echo
+echo
+sleep 5
+
+echo "Step 3 - Verify DENY all is applied"
+echo "-----------------------------------------------------------"
+sleep 3
+
+./login-as.sh emma
+echo
+echo "----------"
+echo
+./check-authz-all.sh DENY user-$LAB_PARTICIPANT_ID-prod-istio-system $OCP_DOMAIN $SSO_CLIENT_SECRET $LAB_PARTICIPANT_ID
+echo
+echo
+echo
+echo
+sleep 2
+
+echo "Step 4 - Authz policy to allow Travel Dashboard UI access"
+echo "-----------------------------------------------------------"
+echo
+echo "Access Dashboard (RBAC: access denied)"
+echo "---------------------------------------"
+curl -k https://travel-user-$LAB_PARTICIPANT_ID.$OCP_DOMAIN/
+echo "----------------"
+sleep 3
+echo
+echo
+echo "apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+  name: authpolicy-istio-ingressgateway
+  namespace: user-${LAB_PARTICIPANT_ID}-prod-istio-system
+spec:
+  selector:
+    matchLabels:
+      app: istio-ingressgateway
+  rules:
+    - to:
+        - operation:
+            paths: [\"*\"]"
+echo
+echo "apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+  name: authpolicy-istio-ingressgateway
+  namespace: user-${LAB_PARTICIPANT_ID}-prod-istio-system
+spec:
+  selector:
+    matchLabels:
+      app: istio-ingressgateway
+  rules:
+    - to:
+        - operation:
+            paths: [\"*\"]" |oc apply -f -
+echo
+echo
+echo
+echo "apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+  name: allow-selective-principals-travel-control
+  namespace: user-$LAB_PARTICIPANT_ID-prod-travel-control
+spec:
+  action: ALLOW
+  rules:
+    - from:
+        - source:
+            principals: [\"cluster.local/ns/user-$LAB_PARTICIPANT_ID-prod-istio-system/sa/istio-ingressgateway-service-account\"]"
+echo
+echo "apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+  name: allow-selective-principals-travel-control
+  namespace: user-$LAB_PARTICIPANT_ID-prod-travel-control
+spec:
+  action: ALLOW
+  rules:
+    - from:
+        - source:
+            principals: [\"cluster.local/ns/user-$LAB_PARTICIPANT_ID-prod-istio-system/sa/istio-ingressgateway-service-account\"]"|oc apply -f -
+
+echo
+echo
+sleep 1
+echo
+echo "Access Dashboard (200)"
+echo "---------------------------------------"
+curl -k https://travel-user-$LAB_PARTICIPANT_ID.$OCP_DOMAIN/
+echo "----------------"
+echo
+echo
+sleep 3
+
+echo "Step 5 - Apply fine grained business Authz policies for service to service communications"
+echo "------------------------------------------------------------------------------------------"
+echo
+./login-as.sh farid
+echo
+echo "----------"
+echo "apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+ name: allow-selective-principals-travel-agency
+ namespace: user-$LAB_PARTICIPANT_ID-prod-travel-agency
+spec:
+ action: ALLOW
+ rules:
+   - from:
+       - source:
+           principals: [\"cluster.local/ns/user-$LAB_PARTICIPANT_ID-prod-istio-system/sa/gto-user-$LAB_PARTICIPANT_ID-ingressgateway-service-account\",\"cluster.local/ns/user-$LAB_PARTICIPANT_ID-prod-travel-agency/sa/default\",\"cluster.local/ns/user-$LAB_PARTICIPANT_ID-prod-travel-portal/sa/default\"]"
+sleep 2
+echo
+echo "apiVersion: security.istio.io/v1beta1
+kind: AuthorizationPolicy
+metadata:
+ name: allow-selective-principals-travel-agency
+ namespace: user-$LAB_PARTICIPANT_ID-prod-travel-agency
+spec:
+ action: ALLOW
+ rules:
+   - from:
+       - source:
+           principals: [\"cluster.local/ns/user-$LAB_PARTICIPANT_ID-prod-istio-system/sa/gto-user-$LAB_PARTICIPANT_ID-ingressgateway-service-account\",\"cluster.local/ns/user-$LAB_PARTICIPANT_ID-prod-travel-agency/sa/default\",\"cluster.local/ns/user-$LAB_PARTICIPANT_ID-prod-travel-portal/sa/default\"]" |oc apply -f -
+
+
+echo "Verify applied business authz policies"
+echo "---------------------------------------"
+sleep 3
+
+./login-as.sh emma
+echo
+echo "----------"
+echo
+./check-authz-all.sh 'ALLOW intra' user-$LAB_PARTICIPANT_ID-prod-istio-system $OCP_DOMAIN $SSO_CLIENT_SECRET $LAB_PARTICIPANT_ID
+echo
+echo
+echo
