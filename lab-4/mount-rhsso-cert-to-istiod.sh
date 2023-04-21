17cf49eb49e78a0d9673b9e03c72b4974cf2d597
skoussou <skousou@gmail.com>
2023-04-21 14:05:33 +0100

    scenario-4 adding script for user lead rhsso cert loading to istiod

---
+++ lab-4/mount-rhsso-cert-to-istiod.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SM_CP_NS=$1
+SM_TENANT_NAME=$2
+BASEDOMAIN=$3
+
+echo '---------------------------------------------------------------------------'
+echo 'ServiceMesh Namespace                      : '$SM_CP_NS
+echo 'ServiceMesh Control Plane Tenant Name      : '$SM_TENANT_NAME
+echo 'OCP Cluster BaseDomain Name                : '$BASEDOMAIN
+echo '---------------------------------------------------------------------------'
+
+echo "Mount the CA secret at the specific location '/cacerts/extra.pem' in istiod pod"
+echo "-----------------------------------------------------------------------------"
+oc set volume -n $SM_CP_NS deployment/istiod-$SM_TENANT_NAME --remove --name=extracacerts --containers=discovery
+echo
+echo
+sleep 2
+echo "oc set volumes -n $SM_CP_NS deployment/istiod-$SM_TENANT_NAME --add  --name=extracacerts  --mount-path=/cacerts  --secret-name=openshift-wildcard  --containers=discovery"
+#sleep 3
+oc set volumes -n $SM_CP_NS deployment/istiod-$SM_TENANT_NAME --add  --name=extracacerts  --mount-path=/cacerts  --secret-name=openshift-wildcard  --containers=discovery
+sleep 5
+echo
+
+echo "Verification of the Procedure"
+echo "-----------------------------------------------------------------------------"
+echo "podname=oc get pods -n $SM_CP_NS | grep istiod-$SM_TENANT_NAME | awk '{print \$1}' | sed '1d'"
+#podname=$(oc get pods -n $SM_CP_NS | grep istiod-$SM_TENANT_NAME | awk '{print $1}'  | awk 'NR>1')
+podname=$(oc get pods -n $SM_CP_NS | grep istiod-$SM_TENANT_NAME | awk '{print $1}' | sed '1d')
+echo
+echo "podname=[$podname]"
+echo
+
+sleep 2
+
+echo
+echo
+echo "Check connection to RHSSO without the CA"
+
+echo "oc -n $SM_CP_NS exec $podname -- cat /cacerts/extra.pem
+sleep 7
+oc -n $SM_CP_NS exec $podname -- cat /cacerts/extra.pem
